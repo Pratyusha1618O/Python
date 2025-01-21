826ee3e7783158ed01fff873b98268a3d37e0712
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2025-01-21 14:52:33 +0530

    File Handling

--- 06_file/08_seek.py
+++ 06_file/08_seek.py
@@ -5,4 +5,5 @@ print("The file pointer is at byte: ", fileptr.tell()) # 0
 #file pointer er location 10 kora holo.    
 fileptr.seek(10)
 print("Now The file pointer is at byte: ", fileptr.tell()) #10
+
 print(fileptr.readline())
